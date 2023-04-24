1e0f260ba5790fe626d3e6bb1f839a8a53a20da9
hmschlng <soulb9@naver.com>
2023-04-24 14:16:05 +0900

    feature: 공통 컴포넌트 추가
    main-serivce 내에서 공통으로 쓰이는 logger, exception handler, respose body
    등의 객체들을 추가했습니다.
    - common/exception/BaseRuntimeException.java
    - common/exception/handler/BaseRuntimeExceptionHandler.java
    - common/logger/RequestAndResponseLoggingAspect.java
    - common/response/BaseResponseBody.java
    
    Issue:

---
+++ backend/main/src/main/java/com/cobby/main/common/exception/BaseRuntimeException.java
@@ -0,0 +1,5 @@
+package com.cobby.main.common.exception;
+
+public class BaseRuntimeException extends RuntimeException {
+
+}
---
+++ backend/main/src/main/java/com/cobby/main/common/logger/RequestAndResponseLoggingAspect.java
@@ -0,0 +1,5 @@
+package com.cobby.main.common.logger;
+
+public class RequestAndResponseLoggingAspect {
+
+}
