8c072923ed61ced2bdea8ef90649306cc3c802fc
hmschlng <soulb9@naver.com>
2023-04-25 17:59:32 +0900

    feat: Costume, AvatarCostume 엔티티와 레포지토리 추가
    Costume 엔티티, Avatar과 Costume의 다대다 매핑을 해결할 AvatarCostume 엔티티,
    그리고 이들 각각의 DB Transaction을 관리할 리포지토리를 추가하였습니다.
    - costume/db/entity/AvatarCostume.java
    - costume/db/entity/Costume.java
    - costume/db/entity/enumtype/CostumeCategory.java
    - costume/db/repository/AvatarCostumeRepository.java
    - costume/db/repository/CostumeRepository.java
    
    Issue: S08P31B201-450

---
+++ backend/main/src/main/java/com/cobby/main/costume/db/entity/AvatarCostume.java
@@ -0,0 +1,45 @@
+package com.cobby.main.costume.db.entity;
+
+import com.cobby.main.avatar.db.entity.Avatar;
+import com.cobby.main.common.entity.BaseTimeEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "avatar_costume")
+@Entity
+public class AvatarCostume extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "avatar_costume_id", nullable = false, columnDefinition = "INT UNSIGNED")
+	private Integer characterCostumeId;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "avatar_id")
+	private Avatar avatar;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "costume_id")
+	private Costume costume;
+
+	public AvatarCostume(Avatar avatar, Costume costume) {
+		this.avatar = avatar;
+		this.costume = costume;
+	}
+}
