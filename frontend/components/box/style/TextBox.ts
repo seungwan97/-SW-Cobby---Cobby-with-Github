4aad93d2a58386a2de029b3c83a33514eefb6773
김성한 <k0s0a7@naver.com>
2023-04-26 14:10:54 +0900

    feat: add ItemBox

--- frontend/components/box/style/TextBox.ts
+++ frontend/components/box/style/TextBox.ts
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const Text = styled.div<{ size: number }>`
   font-size: ${(props) => props.size}px;
+  font-family: "DungGeunMo";
   color: #333333;
 `;
--- frontend/styles/global-style.ts
+++ frontend/styles/global-style.ts
@@ -2,5 +2,12 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+      font-family: "DungGeunMo";
+      src: url("@/fonts/DungGeunMo.ttf");
+    }
+`;
+
+export const colors = createGlobalStyle`
   
 `;
