7fbffe92612b18d7abb227afc2bf32ceea6773da
hmschlng <soulb9@naver.com>
2023-04-27 23:17:05 +0900

    feat: Costume 관련 CRUD 구현
    Costume 관련 CRUD를 구현했습니다.
    - costume/api/controller/CostumeController.java
    - costume/api/service/CostumeService.java
    - costume/api/service/impl/CostumeServiceImpl.java
    
    Costume 관련 요청/응답 DTO를 변경했습니다.
    - costume/api/dto/request/CostumeGetRequest.java
    - costume/api/dto/request/CostumePostRequest.java
    - costume/api/dto/response/CostumeGetResponse.java
    
    Issue: S08P31B201-451

--- backend/main/src/main/java/com/cobby/main/costume/api/service/CostumeService.java
+++ backend/main/src/main/java/com/cobby/main/costume/api/service/CostumeService.java
@@ -1,5 +1,17 @@
 package com.cobby.main.costume.api.service;
 
+import java.util.List;
+
+import com.cobby.main.costume.api.dto.request.CostumePostRequest;
+import com.cobby.main.costume.api.dto.response.CostumeGetResponse;
+
 public interface CostumeService {
 
+	List<CostumeGetResponse> selectAllCostumes();
+
+	CostumeGetResponse selectCostume(Integer costumeId);
+
+	Long insertCostume(CostumePostRequest costumePostRequest);
+
+	Long deleteCostume(Integer costumeId);
 }
