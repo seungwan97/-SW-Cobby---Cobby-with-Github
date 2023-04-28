c58c66874f68fc0c8c92b9bf2cf2b759347f8fe1
배성찬 <scbsoccer@naver.com>
2023-04-28 16:55:39 +0900

    feat: update costume function

---
+++ frontend/components/page/MyPage/MyComponents/style/GithubBadge.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { colors } from "@/styles/colors-style";
+
+export const GithubBadgeWrapper = styled.div`
+  text-align: center;
+`;
+
+export const CodeCopy = styled.div`
+  display: flex;
+  text-align: center;
+  padding: 0 30px;
+`;
+
+export const ReadmeCode = styled.div`
+  background-color: ${colors.NavBarCreamColor};
+  width: 90%;
+`;
+
+export const CopyBtn = styled.div`
+  text-align: center;
+  background-color: ${colors.ItemBoxColor};
+  width: 10%;
+`;
+
+export const CopyBtnImg = styled.div``;
---
+++ frontend/components/page/MyPage/MyComponents/style/MyCobby.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const MyCobbyWrapper = styled.div`
+  text-align: center;
+`;
