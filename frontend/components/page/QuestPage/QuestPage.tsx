c3bc0a6671f5d77db5fe9ec90387f45c40ae5de8
김성한 <k0s0a7@naver.com>
2023-04-28 12:44:21 +0900

    remove: dynamic routing pages

---
+++ frontend/components/page/QuestPage/QuestPage.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const QuestPage = () => {
+  return (
+    <Fragment>
+      <div>QuestPageComponent</div>
+    </Fragment>
+  );
+};
+
+export const getStaticProps = () => {};
+
+export default QuestPage;
