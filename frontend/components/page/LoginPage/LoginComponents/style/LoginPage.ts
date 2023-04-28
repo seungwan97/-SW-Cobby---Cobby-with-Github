e00b1930d4b123ad1f38cf35d75622e04311e55e
김성한 <k0s0a7@naver.com>
2023-04-28 11:20:25 +0900

    update: page layout

--- frontend/components/page/LoginPage/LoginComponents/style/LoginPage.ts
+++ frontend/components/page/LoginPage/LoginComponents/style/LoginPage.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LoginPageContent = styled.div`
   /* 임시  CSS임 => 밑에 NavBar 표시하느라 잠시 넣었음 */
-  margin-top: 50%;
+  margin-top: 30%;
   margin-bottom: 10%;
   padding: 15px;
 `;
