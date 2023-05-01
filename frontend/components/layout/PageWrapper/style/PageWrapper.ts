00099cc844ce1165b8e75685ce713903858edfbb
김성한 <k0s0a7@naver.com>
2023-05-01 16:42:33 +0900

    feat: add responsive for quest page

--- frontend/components/layout/PageWrapper/style/PageWrapper.ts
+++ frontend/components/layout/PageWrapper/style/PageWrapper.ts
@@ -4,14 +4,10 @@ export const PageWrapper = styled.div`
   height: 100%;
   width: 30vw;
 
-  @media (max-width: 767px) {
+  @media (max-width: 1300px) {
     // 모바일
     width: 100%;
     margin-top: 25px;
-
-    @media (max-height: 736px) {
-      /* margin: 0; */
-    }
   }
 
   @media (min-width: 768px) and (max-width: 991px) {
