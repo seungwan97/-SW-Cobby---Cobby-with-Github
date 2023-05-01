6a545ca54ce7ac8837d305a6c0fbec94bd8c829d
hmschlng <soulb9@naver.com>
2023-05-01 14:30:17 +0900

    feat: Request DTO를 record 타입으로 변경
    불변 객체에 대해 record 타입으로 변경하였습니다.
    - costume/api/dto/request/CostumePostRequest.java
    
    Issue: S08P31B201-451

--- backend/main/src/main/java/com/cobby/main/costume/api/dto/request/CostumePostRequest.java
+++ backend/main/src/main/java/com/cobby/main/costume/api/dto/request/CostumePostRequest.java
@@ -1,5 +1,7 @@
 package com.cobby.main.costume.api.dto.request;
 
+import org.springframework.util.Assert;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -7,18 +9,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class CostumePostRequest {
-
+public record CostumePostRequest (
 	@NotBlank(message = "필수 입력 항목입니다. (String)")
-	private String name;
+	String name,
 
 	@NotBlank(message = "필수 입력 항목입니다. (DEFAULT / HEAD / BODY / FACE)")
-	private String category;
+	String category,
 
 	@Positive(message = "필수 입력 항목입니다. (양수)")
 	@Max(value = Integer.MAX_VALUE, message = "범위를 벗어났습니다.")
-	private Integer questId;
+	Integer questId
+){
+
 }
