346c970b537579462871f328fd88995b880f4e95
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-04 19:20:56 +0900

    feat: Security Config 추가

---
+++ backend/auth/src/main/java/com/cobby/auth/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cobby.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()//
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
