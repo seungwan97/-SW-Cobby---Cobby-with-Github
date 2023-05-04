7bbdb0710cb64c9836ee7047be7f790ed08e4392
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-04 19:27:41 +0900

    feat: 인증서버마다 다른 데이터의 구분 처리를 위한 OAuth2Attribute 추가

---
+++ backend/auth/src/main/java/com/cobby/auth/api/service/OAuth2Attribute.java
@@ -0,0 +1,53 @@
+package com.cobby.auth.api.service;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ToString
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+public class OAuth2Attribute {
+    /*
+    OAuth2 인증 후 보내주는 데이터가 각 인증 서버바다 다르기 때문에 별도의 분기 처리가 필요
+    */
+
+    private Map<String, Object> attributes;
+    private String id;
+    private String attributeKey;
+    private String nickname;
+
+    static OAuth2Attribute of(String provider, String attributeKey, Map<String, Object> attributes) {
+
+        switch (provider) {
+            case "github":
+                System.out.println("attributeKey : " + attributeKey); // 1. attributeKey 가 "id" 인지 확인하기
+                return ofGithub("id", attributes);
+            default:
+                throw new RuntimeException();
+        }
+    }
+
+    private static OAuth2Attribute ofGithub(String attributeKey, Map<String, Object> attributes) {
+
+        return OAuth2Attribute.builder()
+                .id(String.valueOf(attributes.get("id")))
+                .nickname((String) attributes.get("login"))
+                .attributes(attributes)
+                .attributeKey(attributeKey)
+                .build();
+    }
+
+    Map<String, Object> convertToMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("key", attributeKey);
+        map.put("nickname", nickname);
+
+        return map;
+    }
+}
