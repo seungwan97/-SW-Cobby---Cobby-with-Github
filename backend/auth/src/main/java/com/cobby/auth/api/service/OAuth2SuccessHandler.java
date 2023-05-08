c53e841ae646279a6688172d4826eb2412653983
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-08 17:47:56 +0900

    feat: OAuth2 인증 후, 서비스 이용을 위한 회원가입 및 회원정보를 저장하는 OAuth2SuccessHandler 구현

---
+++ backend/auth/src/main/java/com/cobby/auth/api/service/OAuth2SuccessHandler.java
@@ -0,0 +1,97 @@
+package com.cobby.auth.api.service;
+
+import com.cobby.auth.api.customenum.Role;
+import com.cobby.auth.api.customenum.TokenKey;
+import com.cobby.auth.api.dto.Token;
+import com.cobby.auth.api.dto.UserDto;
+import com.cobby.auth.api.dto.UserInfoDto;
+import com.cobby.auth.api.entity.User;
+import com.cobby.auth.common.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final MongoTemplate mongoTemplate;
+    private final TokenProvider tokenProvider;
+
+    private String REDIRECT_URI = "http://localhost:3000/login";
+
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        log.info("깃헙 토큰띠 = {}", attributes.get("githubToken"));
+
+        UserDto userDto = UserDto.builder()
+                .oauthId(String.valueOf(attributes.get("id")))
+                .build();
+
+        UserInfoDto userInfoDto;
+
+        User guest = new User();
+
+        Token tokens = new Token();
+
+        // 1. ( Git token ) 을 기준으로 회원 정보를 받아온다.
+        User user = Optional.ofNullable(mongoTemplate.findOne(new Query(Criteria.where("oauthId").is(userDto.getOauthId())),
+                User.class,"user_info")).orElse(guest);
+
+        // 2. 최초 로그인일때, 회원가입( UUID 생성 )을 진행한다.
+        if (user.equals(guest)) {
+            System.out.println("최초 로그인");
+            
+            // 2-1. 회원 정보 저장 ( userDto 의 oauthId 에 해당하는 userId 생성  )
+            mongoTemplate.save(userDto.initUser(userDto));
+
+            // 2-2. oauthId 와 일치하는 user 의 userId 가 담긴 user 객체 불러와 저장 
+            user = Optional.ofNullable(mongoTemplate.findOne(new Query(Criteria.where("oauthId").is(userDto.getOauthId())),
+                    User.class,"user_info")).orElseThrow(NotFoundException::new);
+
+            // 2-3. 전달할 회원 정보 저장
+            userInfoDto = new UserInfoDto(
+                    String.valueOf(user.getUserId()), (String) attributes.get("nickname"), (String) attributes.get("githubUrl"), (String) attributes.get("githubToken")
+            );
+
+            // 2-4. 토큰 발행
+            tokens = tokenProvider.generateToken(userInfoDto.getUserId(), Role.USER.getKey());
+        }
+        else {
+            System.out.println("기존 회원 로그인");
+            userInfoDto = new UserInfoDto(
+                    String.valueOf(user.getUserId()), (String) attributes.get("nickname"), (String) attributes.get("githubUrl"), (String) attributes.get("githubToken")
+            );
+
+            String access = tokenProvider.generateAccess(userInfoDto.getUserId(), Role.USER.getKey());
+
+            tokens = tokenProvider.generateToken(userInfoDto.getUserId(), Role.USER.getKey());
+        }
+
+        String targetUrl;
+        targetUrl = UriComponentsBuilder.fromUriString(REDIRECT_URI)
+                .queryParam(TokenKey.ACCESS.getKey(), "Bearer-" + tokens.getAccessToken())
+                .build().toUriString();
+
+        // 3. Front Page로 리다이렉트 수행
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+    }
+}
