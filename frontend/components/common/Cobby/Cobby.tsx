caa35f85b754e2112a24f10a63a07d5436645dbe
s-ggul <k0s0a7@gmail.com>
2023-05-10 22:51:13 +0900

    feat: add default gifSrc for cobby component

--- frontend/components/common/Cobby/Cobby.tsx
+++ frontend/components/common/Cobby/Cobby.tsx
@@ -6,30 +6,34 @@ type PropsType = {
   gifSrc: string;
 };
 
-const Cobby = (props: PropsType) => {
+const Cobby = ({ gifSrc } : PropsType) => {
   const [cobby, setCobby] = useState("/Character/Cobby2.gif");
   const [cobbyCostume, setCobbyCostume] = useState("");
 
   useEffect(() => {
-    if (props.gifSrc) {
-      setCobbyCostume(props.gifSrc);
+    if (gifSrc) {
+      setCobbyCostume(gifSrc);
       setCobby("/Character/Cobby1.gif");
 
       if (cobby === "/Character/Cobby1.gif") {
         setCobby("/Character/Cobby2.gif");
       } else setCobby("/Character/Cobby1.gif");
     }
-  }, [props.gifSrc]); // 이방법말고는 도저히 불가능한가?
+  }, [gifSrc]); // 이방법말고는 도저히 불가능한가?
 
   return (
     <style.CobbyWrapper>
       <style.Cobby src={cobby} alt={cobbyCostume} />
 
       {cobbyCostume && (
-        <style.CobbyCostumedItem src={props.gifSrc}></style.CobbyCostumedItem>
+        <style.CobbyCostumedItem src={gifSrc}></style.CobbyCostumedItem>
       )}
     </style.CobbyWrapper>
   );
+};
+
+Cobby.defaultProps = {
+  gifSrc : ""
 };
 
 export default Cobby;
