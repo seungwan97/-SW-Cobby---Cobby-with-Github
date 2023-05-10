89ff05a26b642fd39d9af725b5d155843fb54c40
김대은 <eodms090@naver.com>
2023-05-10 17:36:09 +0900

    fix: user login 수정
    login 수정하였습니다.
    
    Issue:

--- backend/user/src/main/java/com/cobby/main/user/api/service/impl/UserServiceImpl.java
+++ backend/user/src/main/java/com/cobby/main/user/api/service/impl/UserServiceImpl.java
@@ -89,11 +89,9 @@ public class UserServiceImpl implements UserService {
 				.build();
 			userRepository.save(user);
 
-			var user1 = userRepository.findById(userPostRequest.userId()).orElseThrow(NotFoundException::new);
-
 			// 새로운 사용자의 통계 정보를 생성합니다.
 			Stat stat = Stat.builder()
-				.user(user1)
+				.user(user)
 				.commitCnt(getStatList(userPostRequest, 5))
 				.starCnt(getStatList(userPostRequest, 3))
 				.prCnt(getStatList(userPostRequest, 7))
