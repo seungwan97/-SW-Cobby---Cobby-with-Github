c6dd3e1e6fb2cd8b386ad4264aec112dbf8a6c2d
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-11 10:18:22 +0900

    feat: 토큰 만료 시 JWT 토큰 재발급 로직 추가

--- backend/auth/src/main/java/com/cobby/auth/common/filter/JwtAuthFilter.java
+++ backend/auth/src/main/java/com/cobby/auth/common/filter/JwtAuthFilter.java
@@ -3,8 +3,10 @@ package com.cobby.auth.common.filter;
 import com.cobby.auth.api.customenum.JwtCode;
 import com.cobby.auth.api.customenum.Role;
 import com.cobby.auth.api.customenum.TokenKey;
+import com.cobby.auth.api.dto.Token;
 import com.cobby.auth.api.dto.UserDto;
 import com.cobby.auth.api.service.TokenProvider;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -33,7 +35,7 @@ public class JwtAuthFilter extends GenericFilterBean {
 
         log.info("token = {}", token);
 
-        if(token != null && tokenProvider.validateToken(token) ==  JwtCode.ACCESS) {
+        if (token != null && tokenProvider.validateToken(token) == JwtCode.ACCESS) { // Jwt Token 만료되지 않았을 때,
             String userId = tokenProvider.getUid(token);
 
             UserDto userDto = UserDto.builder()
@@ -45,6 +47,28 @@ public class JwtAuthFilter extends GenericFilterBean {
 
             log.info("set Authentication to security context for '{}', uri = {}", auth.getName(), ((HttpServletRequest)request).getRequestURI());
 
+        }
+        else if (token != null && tokenProvider.validateToken(token) == JwtCode.EXPIRED) { // Jwt Token 이 만료되었을 때,
+            Claims claims = tokenProvider.getClaims(token);
+
+            // 토큰에 저장된 유저정보 추출
+            UserDto userDto = UserDto.builder()
+                    .userId(claims.getSubject())
+                    .build();
+
+            // 헤더에 존재하는 리프레시 토큰
+            String refresh = tokenProvider.resolveToken(
+                    ((HttpServletRequest)request).getHeader(TokenKey.REFRESH.getKey()));
+
+            // 캐시에 존재하는 리프레시 토큰
+            String savedRefresh = tokenProvider.getSavedRefresh(userDto.getUserId());
+            
+            // Refresh Token 을 확인해서 Access Token 재발급
+            if (token != null && refresh.equals(savedRefresh) && tokenProvider.validateToken(refresh) == JwtCode.ACCESS) {
+                Token tokens = tokenProvider.generateToken(userDto.getUserId(), Role.USER.getKey());
+
+                tokenProvider.setSaveRefresh(userDto.getUserId(), tokens.getRefreshToken(), tokenProvider.getExpiration(TokenKey.REFRESH));
+            }
         }
         else {
             log.info("no valid JWT token found, uri: {}", ((HttpServletRequest)request).getRequestURI());
