ea42fd9cd2a37e2d4d6873c7bebf63faa9bedb71
seungwan97 <younhaholic97@gmail.com>
2023-05-11 09:00:16 +0900

    feat : 230511 어제일자 커밋

--- frontend/pages/api/users.ts
+++ frontend/pages/api/users.ts
@@ -13,30 +13,3 @@ export const getNickname = async (accessToken: string, userId: string) => {
 
   return response;
 };
-
-// 메인페이지 github status 정보 불러오기
-export const getCommitInfo = async (accessToken: string, userId: string) => {
-  const response = await client.get(`/api/activityLog/commit`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      userId: `${userId}`,
-    },
-  });
-
-  return response;
-};
-
-// 메인페이지 github status 정보 불러오기
-export const getAttendanceInfo = async (
-  accessToken: string,
-  userId: string
-) => {
-  const response = await client.get(`/api/activityLog/attendance`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      userId: `${userId}`,
-    },
-  });
-
-  return response;
-};
