6ccb683b1b5ab6cd349a74d0be7d2a8a00d3dac5
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-12 10:58:57 +0900

    feat: FE에서 토큰정보 요청에 대한 응답 기능 추가

--- backend/auth/src/main/java/com/cobby/auth/api/controller/AuthController.java
+++ backend/auth/src/main/java/com/cobby/auth/api/controller/AuthController.java
@@ -1,20 +1,22 @@
 package com.cobby.auth.api.controller;
 
+import com.cobby.auth.common.model.BaseResponseBody;
+import com.cobby.auth.common.util.ApiDocumentResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "인증 정보", description = "OAuth2 인증 정보 API 문서입니다.")
 public class AuthController {
-
-    @GetMapping("/{token}")
-    boolean checkAccessToken(@PathVariable String token) {
-        log.info("token = {}", token);
-        return true;
-    }
 
     @PostMapping
     boolean webhookTest() {
@@ -22,13 +24,29 @@ public class AuthController {
         return true;
     }
 
+    @GetMapping("/data")
+    @ApiDocumentResponse
+    @Operation(summary = "토큰 조회", description = "Request Header 에 있는 Access Token 값을 반환하는 메서드 입니다.")
+    public ResponseEntity<? extends BaseResponseBody> getTokenData(@RequestHeader("Authorization") String token) {
+
+        return ResponseEntity
+                .ok()
+                .body(new BaseResponseBody<>(200, "Access Token fetched successfully", token));
+    }
+
     /**
      * Refresh Token
      * FE 에서 Access Token 확인 후, 만료 되었을 때 오는 Re:issue 요청
     **/
-//    @PostMapping(value = "/reissue")
-//    public ResponseEntity<?> reissue(HttpServletRequest httpServletRequest) {
-//
-//    }
+    @GetMapping(value = "/reissue")
+    @ApiDocumentResponse
+    @Operation(summary = "토큰 조회", description = "Request Header 에 있는 쿠키의 Refresh Token 값을 반환하는 메서드 입니다.")
+    public ResponseEntity<? extends BaseResponseBody> reissue(@RequestHeader("Cookie") String token) {
+
+        return ResponseEntity
+                .ok()
+                .body(new BaseResponseBody<>(200, "Refresh Token fetched successfully", token));
+
+    }
 
 }
