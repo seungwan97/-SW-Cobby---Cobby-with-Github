e8eaef582ae1db77dbd245187340b79c3f9d2926
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-12 10:59:29 +0900

    feat: 서버 요청에대한 기본 응답값 정의 및 추가

--- backend/auth/src/main/java/com/cobby/auth/common/model/BaseResponseBody.java
+++ backend/auth/src/main/java/com/cobby/auth/common/model/BaseResponseBody.java
@@ -1,32 +1,25 @@
 package com.cobby.auth.common.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 /**
  * 서버 요청에대한 기본 응답값(바디) 정의.
+ * 응답 컨텐츠에 상태값을 함께 담아서 주기 위해 ResponseBody 에 들어갈 공통 body 양식을 정의한 클래스입니다. 구성 요소는 다음과 같습니다. - status:
+ * 상태 코드 넘버 (ex. 200) - naming:  상태 코드 이름 - content: 컨텐츠
+ *
+ * @param <T>
  */
-@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
-public class BaseResponseBody {
-    String message = null;
-    Integer statusCode = null;
+public class BaseResponseBody<T> {
+    private Integer statusCode;
 
-    public BaseResponseBody() {}
+    private String message;
 
-    public BaseResponseBody(Integer statusCode){
-        this.statusCode = statusCode;
-    }
+    private T content;
 
-    public BaseResponseBody(Integer statusCode, String message){
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-
-    public static BaseResponseBody of(Integer statusCode, String message) {
-        BaseResponseBody body = new BaseResponseBody();
-        body.message = message;
-        body.statusCode = statusCode;
-        return body;
-    }
 }
