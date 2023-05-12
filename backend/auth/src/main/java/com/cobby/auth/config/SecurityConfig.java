daa1e0e46f2c630994a283f7251013075be5bb6a
MJun111 <kmj0568@gmail.com>
2023-05-12 22:58:24 +0900

    chore: swagger https 설정 추가

--- backend/auth/src/main/java/com/cobby/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/com/cobby/auth/config/SecurityConfig.java
@@ -6,10 +6,12 @@ import com.cobby.auth.api.service.OAuth2SuccessHandler;
 import com.cobby.auth.api.service.TokenProvider;
 import com.cobby.auth.common.filter.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +24,21 @@ public class SecurityConfig {
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler successHandler;
     private final TokenProvider tokenProvider;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/favicon.ico",
+                        "/error",
+                        "/swagger-resources/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs",
+                        "/webjars/**"
+                )
+                .and()
+                .ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());    // 정적인 리소스들에 대해서 시큐리티 적용 무시.
+    }
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
