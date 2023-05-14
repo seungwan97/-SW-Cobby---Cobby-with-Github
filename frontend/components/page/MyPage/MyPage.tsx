f380d0b47c1f5ef2e6f8361013e37977d13f53c4
s-ggul <k0s0a7@gmail.com>
2023-05-14 21:21:06 +0900

    feat: add modal for mypage

--- frontend/components/page/MyPage/MyPage.tsx
+++ frontend/components/page/MyPage/MyPage.tsx
@@ -1,6 +1,6 @@
 // 유저별 마이페이지
 import { useRouter } from "next/router";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import BottomNavBar from "@/components/layout/BottomNavBar/BottomNavBar";
 import * as page from "@/components/layout/PageWrapper/style/PageWrapper";
 import TextBox from "@/components/common/TextBox/TextBox";
@@ -9,6 +9,7 @@ import MyCobby from "./MyComponents/MyCobby";
 import GithubBadge from "./MyComponents/GithubBadge";
 import UserInformation from "./MyComponents/UserInformation";
 import LogoutBtn from "./MyComponents/LogoutBtn";
+import Modal from "@/components/common/Modal/Modal";
 
 interface MyFuncProps {
   nickname: string;
@@ -28,8 +29,25 @@ const MyPage = ({
 }: MyFuncProps) => {
   // const router = useRouter();
 
+  const [isLogout, setIsLogout] = useState(false);
+  const [isLeave, setIsLeave] = useState(false);
+
+  const setLogout = (val: boolean) => {
+    setIsLogout(val);
+  }
+
+  const setLeave = () => {
+    setIsLeave(true);
+  }
+
   return (
     <Fragment>
+      {isLogout && (
+        <Modal name={""} name2={"로그아웃 하시겠습니까?"} yes={true} no={true} setStatus={setIsLogout} />
+      )}
+      {isLeave && (
+        <Modal name={""} name2={"회원탈퇴 하시겠습니까?"} yes={true} no={true} setStatus={setIsLeave} />
+      )}
       <page.PageWrapper>
         <style.MyPageTxt>
           <TextBox size={50} content={"MY PAGE"} />
@@ -43,8 +61,8 @@ const MyPage = ({
           cntCostumes={cntCostumes}
           cntQuests={cntQuests}
         />
-        <LogoutBtn />
-        <style.LeaveButton>
+        <LogoutBtn setLogout={setLogout} />
+        <style.LeaveButton onClick={setLeave}>
           <TextBox size={20} content={"Leave Our App"} />
           {/* 이거 LeaveButton에 대해서 반응형 처리 해줘야함. */}
         </style.LeaveButton>
