f40a4de3c05e355f908d3d3fe8eca5831ea1e86a
dbsxowns2 <dbsxowns34@gmail.com>
2023-05-15 11:25:30 +0900

    feat: Security 설정 및 적용

---
+++ backend/main/src/main/java/com/cobby/main/common/authorization/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.cobby.main.common.authorization;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private CustomAuthorizationFilter customAuthorizationFilter;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/favicon.ico",
+                        "/error",
+                        "/swagger-resources/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs",
+                        "/webjars/**"
+                )
+                .and()
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());    // 정적인 리소스들에 대해서 시큐리티 적용 무시.
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
