61d32a74c124767ea79fd1e471899c2172ccee84
김명준 <kmj6359@naver.com>
2023-05-17 10:39:38 +0900

    chore: 시큐리티 설정 추가

--- backend/user/src/main/java/com/cobby/main/common/authorization/CustomAuthorizationFilter.java
+++ backend/user/src/main/java/com/cobby/main/common/authorization/CustomAuthorizationFilter.java
@@ -36,6 +36,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         if (request.getServletPath().contains("health")
                 || request.getServletPath().equals("/api/user/users")
                 || request.getServletPath().contains("swagger")
+                || request.getServletPath().contains("activityLog")
                 || request.getServletPath().contains("webhooks")
                 || request.getServletPath().contains("api-docs")
                 || request.getServletPath().contains("/badge/")
